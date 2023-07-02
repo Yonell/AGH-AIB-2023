93886dd504ec309f1b4a344a0ed880f2e7b6fa69
Filip Piskorski <fpfpfp2002@gmail.com>
2023-07-02 22:04:41 +0200

    repaired misspells

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
               str(time.time() - start_time))
         print("Average fitness: " + str(np.average([x.get_fitness() for x in population.population])))
         print("Best ever fitness: " + str(population.get_best_ever_fitness()))
-        NeuralNetworkFactory().save_neural_network_to_file(population.get_best_ever(), "./best_neural_network.picle")
+        NeuralNetworkFactory().save_neural_network_to_file(population.get_best_ever(), "./best_neural_network.pickle")
     print(population.population[0].get_fitness())
     population.population[0].fitness = None
     exit(0)
